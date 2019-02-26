4ca028637717a72c0f72f67727465e593e7a1c1d
ricardobah <rickribeiro99@gmail.com>
2019-02-26 14:09:41 -0300

    finish project and add cors

--- server.js
+++ server.js
@@ -4,8 +4,10 @@
 //aula 5
 const express = require('express');
 const mongoose = require ('mongoose');
+const cors = require("cors")
 const app = express();
 app.use(express.json());
+app.use(cors()) // Estudar cors
 const requireDir = require('require-dir');
 mongoose.connect('mongodb://127.0.0.1:27017/nodeapi', {useNewUrlParser: true})
 requireDir('./src/models')
