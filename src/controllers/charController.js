93c0f1b749bd327c34725a4269042146067a8fab
ricardobah <rickribeiro99@gmail.com>
2019-02-26 13:39:44 -0300

    remove and edit

--- src/controllers/charController.js
+++ src/controllers/charController.js
@@ -6,10 +6,21 @@ module.exports = {
         const char = await Char.find()
         return res.json(char)
     },
-
+     async show(req,res){
+            const char = await Char.findById(req.params.id)
+            return res.json(char)
+     }, 
     async store(req,res){
         const char = await Char.create(req.body);
         return res.json(char)
+    },
+    async update(req,res){
+        const char = await Char.findByIdAndUpdate(req.params.id,req.body,{new: true})
+        return res.json(char)
+    },
+    async destroy(req,res){
+        const char = await Char.findByIdAndRemove(req.params.id)
+        return res.send()
     }
 
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,7 @@ const routes = express.Router();
 const CharController = require("./controllers/charController")
 routes.get("/char", CharController.index)
 routes.post("/char", CharController.store)
-
+routes.get("/char/:id", CharController.show)
+routes.put("/char/:id", CharController.update)
+routes.delete("/char/:id", CharController.destroy)
 module.exports = routes;
